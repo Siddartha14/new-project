33bb3d7a4f75e6d4fd632abe3fc4d39c59ff36f2
Siddu Reddy <mallareddyp049@gmail.com>
2023-05-30 17:09:18 +0530

    first commit

---
+++ src/main/java/com/gitproject/git/App.java
@@ -0,0 +1,27 @@
+package com.gitproject.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void add(String data) {
+		System.out.println("dummy method");
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public void run() {
+    	System.out.println("hihjshsjhsjsh");
+    }
+    public void gi() {
+    	System.out.println("jjdshjshsj");
+    }
+    public void gii() {
+    	System.out.println("jjdshjshsj");
+    }
+    
+    
+}
